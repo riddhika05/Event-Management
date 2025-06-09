c42aaed7ef32e22d94985365617c6a37f60a06c9
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-06-09 21:59:06 +0530

    Added buttons for explore and host event

---
+++ EventSphere/Controllers/Events.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventSphere.Controllers
+{
+    public class Events : Controller
+    {
+        public IActionResult Explore()
+        {
+            return View();
+        }
+
+        public IActionResult Host()
+        {
+            return View();
+        }
+    }
+}
--- EventSphere/obj/Debug/net8.0/EventSphere.AssemblyInfo.cs
+++ EventSphere/obj/Debug/net8.0/EventSphere.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a90cfb493fa42c39944563fc0b77dc4a0b3fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6f57f74a6cfde59af7a59120fabd2a02d91728")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
