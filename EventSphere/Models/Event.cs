9c6d5d9e2a4479c34cadc28dbbe0bc37b02121f7
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-06-09 23:52:20 +0530

    Added an image slider to home page

---
+++ EventSphere/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace EventSphere.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Organizer { get; set; }
+        public int AttendeeCount { get; set; }
+      
+    }
+}
